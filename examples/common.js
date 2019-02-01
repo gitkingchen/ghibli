ed4c21dd7ce58cdc99df0219eec91a9ea6084770
jinchen-s <jinchen-s@360.cn>
2019-02-01 18:13:34 +0800

    add image-text

---
+++ examples/common.js
@@ -0,0 +1,16 @@
+
+$(function() {
+	var $ele = $('.demo6 .des');
+	var maxH = 44;
+	if($ele.height() <= maxH) {
+		$ele.height("auto");
+	} else {
+		while($ele.height() > maxH){
+			$ele.text($ele.text().replace(/(\s)*([a-zA-Z0-9]+|\W)(\.\.\.)?$/, "..."));
+		}
+	}
+
+	$ele.css('visibility','visible');
+
+});
+
