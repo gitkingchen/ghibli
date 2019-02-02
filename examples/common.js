cff057570dee7b7081a552b24870e11dd413ebdf
jinchen-s <jinchen-s@360.cn>
2019-02-02 19:48:03 +0800

    add many close btn

--- examples/common.js
+++ examples/common.js
@@ -12,5 +12,28 @@ $(function() {
 
 	$ele.css('visibility','visible');
 
+	/*canvas 绘制关闭按钮*/
+	function close_button(x,y,side){
+		ctx.fillStyle="#fff";
+		ctx.fillRect(x, y, side, side);
+
+		ctx.beginPath();
+		ctx.moveTo(0 , 0 );
+		ctx.lineTo(50 , 50 );
+		ctx.moveTo(50 , 0 );
+		ctx.lineTo(0 , 50 );
+		ctx.strokeStyle = '#000';
+		ctx.lineWidth = 4;
+		ctx.stroke();
+	}
+
+	var c = document.getElementById("close-canvas");
+	var ctx = c.getContext("2d");
+	c.width = 50;
+	c.height = 50;
+
+	close_button(0, 0, 50);
+	/*canvas 绘制关闭按钮*/
+
 });
 
