1a61c0bfecfd870602a4163c1454200c187178c8
jinchen-s <jinchen-s@360.cn>
2019-05-21 19:15:50 +0800

    image-text import vue

--- src/main.js
+++ src/main.js
@@ -1,29 +1,28 @@
-import Vue from 'vue'
+import Vue from "vue";
 
-import 'normalize.css/normalize.css' // A modern alternative to CSS resets
+import "normalize.css/normalize.css"; // A modern alternative to CSS resets
+import "@/styles/reset.css";
 
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-import locale from 'element-ui/lib/locale/lang/zh-CN' // lang i18n
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+import locale from "element-ui/lib/locale/lang/zh-CN"; // lang i18n
 
-import '@/styles/index.scss' // global css
+import "@/styles/index.scss"; // global css
+import App from "./App";
+import router from "./router";
+import store from "./store";
+import $ from "jquery";
 
-import App from './App'
-import router from './router'
-import store from './store'
-
-
-import '@/icons' // icon
-import '@/permission' // permission control
+import "@/icons"; // icon
+import "@/permission"; // permission control
 
 Vue.config.productionTip = false;
 
-Vue.use(ElementUI, { locale })
+Vue.use(ElementUI, { locale });
 
 new Vue({
-  el: '#app',
+  el: "#app",
   router,
   store,
   render: h => h(App)
-})
-
+});
--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -29,53 +29,63 @@ const user = {
     Login({ commit }, userInfo) {
       const username = userInfo.username.trim()
       return new Promise((resolve, reject) => {
-        login(username, userInfo.password).then(response => {
-          const data = response.data
-          setToken(data.token)
-          commit('SET_TOKEN', data.token)
-          resolve()
-        }).catch(error => {
-          reject(error)
-        })
+        // login(username, userInfo.password).then(response => {
+        //   const data = response.data
+        //   setToken(data.token)
+        //   commit('SET_TOKEN', data.token)
+        //   resolve()
+        // }).catch(error => {
+        //   reject(error)
+        // })
+        
+        setToken('towertoken')
+        commit('SET_TOKEN', 'towertoken')
+        resolve()
       })
     },
 
     // 获取用户信息
     GetInfo({ commit, state }) {
       return new Promise((resolve, reject) => {
-        getInfo(state.token).then(response => {
-          const data = response.data
+        // getInfo(state.token).then(response => {
+        //   const data = response.data
           //if (data.roles && data.roles.length > 0) { // 验证返回的roles是否是一个非空数组
           
-            if(data.user_name){
-              data.roles = ['test'];//某一天需要服务端改返回的角色
-              commit('SET_ROLES', data.roles)
-            }
+            // if(data.user_name){
+            //   data.roles = ['test'];//某一天需要服务端改返回的角色
+            //   commit('SET_ROLES', data.roles)
+            // }
           
           
           //} else {
           //  reject('getInfo: roles must be a non-null array !')
           //}
-          commit('SET_NAME', data.user_name)
+          //commit('SET_NAME', data.user_name)
           commit('SET_AVATAR', 'https://wpimg.wallstcn.com/f778738c-e4f8-4870-b634-56703b4acafe.gif')
-          resolve(response)
-        }).catch(error => {
-          reject(error)
-        })
+          resolve()
+          //resolve(response)
+        // }).catch(error => {
+        //   reject(error)
+        // })
       })
     },
 
     // 登出
     LogOut({ commit, state }) {
       return new Promise((resolve, reject) => {
-        logout(state.token).then(() => {
-          commit('SET_TOKEN', '')
-          commit('SET_ROLES', [])
-          removeToken()
-          resolve()
-        }).catch(error => {
-          reject(error)
-        })
+        // logout(state.token).then(() => {
+        //   commit('SET_TOKEN', '')
+        //   commit('SET_ROLES', [])
+        //   removeToken()
+        //   resolve()
+        // }).catch(error => {
+        //   reject(error)
+        // })
+        // 
+        commit('SET_TOKEN', '')
+        commit('SET_ROLES', [])
+        removeToken()
+        resolve()
       })
     },
 
