4d20c0d1064a9aa412243927eca6841395e365a0
jinchen-s <jinchen-s@360.cn>
2019-08-27 14:54:02 +0800

    '迁移'

--- src/router/index.js
+++ src/router/index.js
@@ -41,7 +41,6 @@ export const constantRouterMap = [
     //   component: () => import('@/views/quicklayout/index')
     // }]
   },
-
   {
     path: "/quicklayout",
     component: Layout,
@@ -58,16 +57,204 @@ export const constantRouterMap = [
         name: "Image-Text",
         component: () => import("@/views/quicklayout/image-text"),
         meta: { title: "image-text", icon: "form" }
+      },
+      {
+        path: "icon-text",
+        name: "Icon-Text",
+        component: () => import("@/views/quicklayout/icon-text"),
+        meta: { title: "icon-text", icon: "form" }
+      },
+      {
+        path: "css3-scrollbar",
+        name: "Css3-scrollbar",
+        component: () => import("@/views/quicklayout/css3-scrollbar"),
+        meta: { title: "css3-scrollbar", icon: "form" }
+      },
+      {
+        path: "backtop",
+        component: () => import("@/views/quicklayout/backtop/index"), // Parent router-view
+        name: "backtop",
+        meta: { title: "backtop(0/anchor)", icon: "form" },
+        redirect: "/quicklayout/backtop/backtop-1",
+        children: [
+          {
+            path: "backtop-1",
+            component: () => import("@/views/quicklayout/backtop/backtop-1"),
+            name: "backtop-1",
+            meta: { title: "backtop-1", icon: "form" }
+          },
+          {
+            path: "backtop-2",
+            component: () => import("@/views/quicklayout/backtop/backtop-2"),
+            name: "backtop-2",
+            meta: { title: "backtop-2", icon: "form" }
+          },
+          {
+            path: "backtop-3",
+            component: () => import("@/views/quicklayout/backtop/backtop-3"),
+            name: "backtop-3",
+            meta: { title: "backtop-3", icon: "form" }
+          },
+          {
+            path: "backtop-4",
+            component: () => import("@/views/quicklayout/backtop/backtop-4"),
+            name: "backtop-4",
+            meta: { title: "backtop-4", icon: "form" }
+          },
+          {
+            path: "backtop-5",
+            component: () => import("@/views/quicklayout/backtop/backtop-5"),
+            name: "backtop-5",
+            meta: { title: "backtop-5", icon: "form" }
+          },
+          {
+            path: "backtop-6",
+            component: () => import("@/views/quicklayout/backtop/backtop-6"),
+            name: "backtop-6",
+            meta: { title: "backtop-6", icon: "form" }
+          },
+          {
+            path: "backtop-7",
+            component: () => import("@/views/quicklayout/backtop/backtop-7"),
+            name: "backtop-7",
+            meta: { title: "backtop-7", icon: "form" }
+          }
+        ]
+      },
+      {
+        path: "loading",
+        component: () => import("@/views/quicklayout/loading/index"), // Parent router-view
+        name: "loading",
+        meta: { title: "loading", icon: "form" },
+        redirect: "/quicklayout/loading/loading1",
+        children: [
+          {
+            path: "loading1",
+            component: () => import("@/views/quicklayout/loading/loading1"),
+            name: "loading1",
+            meta: { title: "loading1", icon: "form" }
+          },
+          {
+            path: "loading2",
+            component: () => import("@/views/quicklayout/loading/loading2"),
+            name: "loading2",
+            meta: { title: "loading2", icon: "form" }
+          },
+          {
+            path: "loading3",
+            component: () => import("@/views/quicklayout/loading/loading3"),
+            name: "loading3",
+            meta: { title: "loading3", icon: "form" }
+          }
+        ]
+      },
+      {
+        path: "close",
+        name: "close",
+        component: () => import("@/views/quicklayout/close"),
+        meta: { title: "close", icon: "form" }
+      },
+      {
+        path: "container-center",
+        name: "container-center",
+        component: () => import("@/views/quicklayout/container-center"),
+        meta: { title: "container-center", icon: "form" }
+      },
+      {
+        path: "css3-selector",
+        name: "css3-selector",
+        component: () => import("@/views/quicklayout/css3-selector"),
+        meta: { title: "css3-selector", icon: "form" }
       }
-      // {
-      //   path: 'icon-text',
-      //   name: 'Icon-Text',
-      //   component: () => import('@/views/quicklayout/icon-text'),
-      //   meta: { title: 'icon-text', icon: 'form' }
-      // }
     ]
   },
-
+  {
+    path: "/toolFn",
+    component: Layout,
+    redirect: "/toolFn/date",
+    name: "工具函数",
+    meta: {
+      title: "工具函数",
+      icon: "nested"
+    },
+    alwaysShow: true,
+    children: [
+      {
+        path: "date",
+        name: "date",
+        component: () => import("@/views/toolFn/date"),
+        meta: { title: "date", icon: "form" }
+      },
+      {
+        path: "operate",
+        name: "operate",
+        component: () => import("@/views/toolFn/operate"),
+        meta: { title: "operate", icon: "form" }
+      },
+      {
+        path: "throttle",
+        name: "throttle",
+        component: () => import("@/views/toolFn/throttle"),
+        meta: { title: "throttle", icon: "form" }
+      },
+      {
+        path: "debounce",
+        name: "debounce",
+        component: () => import("@/views/toolFn/debounce"),
+        meta: { title: "debounce", icon: "form" }
+      }
+    ]
+  },
+  {
+    path: "/animation",
+    component: Layout,
+    redirect: "/animation/base",
+    name: "animation",
+    meta: {
+      title: "动画操作",
+      icon: "nested"
+    },
+    alwaysShow: true,
+    children: [
+      {
+        path: "base",
+        name: "base",
+        component: () => import("@/views/animation/base"),
+        meta: { title: "base", icon: "form" }
+      },
+      {
+        path: "setTimeout",
+        name: "setTimeout",
+        component: () => import("@/views/animation/setTimeout"),
+        meta: { title: "setTimeout", icon: "form" }
+      },
+      {
+        path: "requestAnimationFrame",
+        name: "requestAnimationFrame",
+        component: () => import("@/views/animation/requestAnimationFrame"),
+        meta: { title: "requestAnimation", icon: "form" }
+      }
+    ]
+  },
+  {
+    path: "/regular",
+    component: Layout,
+    redirect: "/regular/base",
+    name: "regular",
+    meta: {
+      title: "正则",
+      icon: "nested"
+    },
+    alwaysShow: true,
+    children: [
+      {
+        path: "base",
+        name: "base",
+        component: () => import("@/views/regular/base"),
+        meta: { title: "base", icon: "form" }
+      }
+    ]
+  },
   {
     path: "/tools",
     component: Layout,
@@ -80,6 +267,66 @@ export const constantRouterMap = [
       }
     ]
   },
+  {
+    path: "/blogs",
+    component: Layout,
+    children: [
+      {
+        path: "index",
+        name: "常看博客",
+        component: () => import("@/views/blogs/index"),
+        meta: { title: "常看博客", icon: "form" }
+      }
+    ]
+  },
+  {
+    path: "/plugins",
+    component: Layout,
+    children: [
+      {
+        path: "index",
+        name: "常用插件",
+        component: () => import("@/views/plugins/index"),
+        meta: { title: "常用插件", icon: "form" }
+      }
+    ]
+  },
+  {
+    path: "/creativeLab",
+    component: Layout,
+    children: [
+      {
+        path: "index",
+        name: "创意实验室",
+        component: () => import("@/views/creativeLab/index"),
+        meta: { title: "创意实验室", icon: "form" }
+      }
+    ]
+  },
+  {
+    path: "/myTracks",
+    component: Layout,
+    children: [
+      {
+        path: "index",
+        name: "我的足迹",
+        component: () => import("@/views/myTracks/index"),
+        meta: { title: "我的足迹", icon: "form" }
+      }
+    ]
+  },
+  {
+    path: "external-link",
+    component: Layout,
+    children: [
+      {
+        path: "https://github.com/gitkingchen",
+        name: "开源项目",
+        meta: { title: "开源项目", icon: "form" }
+      }
+    ]
+  },
+  
 
   { path: "*", redirect: "/404", hidden: true }
 ];
---
+++ src/views/quicklayout/css3-scrollbar.vue
@@ -0,0 +1,186 @@
+<template>
+  <div class="app-container">
+    <!--priview-->
+    <div>
+      <strong>css3 滚动条1</strong>
+      <div class="demo9_1">
+        我是内容<br />我是内容<br />我是内容<br />我是内容<br />我是内容<br />我是内容<br />我是内容<br />我是内容<br />
+        我是内容<br />我是内容<br />我是内容<br />我是内容<br />我是内容<br />我是内容<br />我是内容<br />我是内容<br />
+      </div>
+    </div>
+    <!--priview-->
+    <!--code-->
+    <div style="width:800px;">
+      <script
+        type="text/plain"
+        class="language-markup"
+      >
+        <div class="demo9_1">
+            我是内容<br />我是内容<br />我是内容<br />我是内容<br />我是内容<br />我是内容<br />我是内容<br />我是内容<br />
+            我是内容<br />我是内容<br />我是内容<br />我是内容<br />我是内容<br />我是内容<br />我是内容<br />我是内容<br />
+        </div>
+      </script>
+
+      <pre><code class="language-scss">
+        .demo9_1{
+            width: 200px;
+            max-height: 200px;
+            overflow: auto;
+        }
+
+        .demo9_1 {
+            &::-webkit-scrollbar {
+                width: 10px;
+                background-color: #f5f5f5;
+            }
+            &::-webkit-scrollbar-track {
+                border-radius: 10px;
+                -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+                box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.3);
+                background-color: #f5f5f5;
+            }
+            &::-webkit-scrollbar-thumb {
+                border-radius: 10px;
+                background-color: #555;
+            }
+        }
+      </code></pre>
+    </div>
+    <!--code-->
+
+    <!--priview-->
+    <div>
+      <strong>css3 滚动条2</strong>
+      <div class="demo9_2">
+        我是内容<br />我是内容<br />我是内容<br />我是内容<br />我是内容<br />我是内容<br />我是内容<br />我是内容<br />
+        我是内容<br />我是内容<br />我是内容<br />我是内容<br />我是内容<br />我是内容<br />我是内容<br />我是内容<br />
+      </div>
+    </div>
+    <!--priview-->
+    <!--code-->
+    <div style="width:800px;">
+      <script
+        type="text/plain"
+        class="language-markup"
+      >
+        <div class="demo9_2">
+            我是内容<br/>我是内容<br/>我是内容<br/>我是内容<br/>我是内容<br/>我是内容<br/>我是内容<br/>我是内容<br/>
+            我是内容<br/>我是内容<br/>我是内容<br/>我是内容<br/>我是内容<br/>我是内容<br/>我是内容<br/>我是内容<br/>
+        </div>
+      </script>
+
+      <pre><code class="language-scss">
+        .demo9_2{
+            width: 200px;
+            max-height: 200px;
+            overflow: auto;
+        }
+
+        .demo9_2 {
+            &::-webkit-scrollbar {
+                width: 10px;
+                background-color: #f5f5f5;
+            }
+            &::-webkit-scrollbar-thumb {
+                border-radius: 10px;
+                background: linear-gradient(to left, #96a6bf, #63738c);
+                -webkit-box-shadow: inset 0 0 1px 1px #5c6670;
+                box-shadow: inset 0 0 1px 1px #5c6670;
+            }
+            &::-webkit-scrollbar-track {
+                border-radius: 10px;
+                background: #eee;
+                -webkit-box-shadow: 0 0 1px 1px #bbb, inset 0 0 7px rgba(0,0,0,0.3);
+                box-shadow: 0 0 1px 1px #bbb, inset 0 0 7px rgba(0,0,0,0.3);
+            }
+        }
+      </code></pre>
+
+    </div>
+    <!--code-->
+
+    <!--priview-->
+    <div>
+      <strong>css3 滚动条3</strong>
+      <div class="demo9_3">
+        我是内容<br />我是内容<br />我是内容<br />我是内容<br />我是内容<br />我是内容<br />我是内容<br />我是内容<br />
+        我是内容<br />我是内容<br />我是内容<br />我是内容<br />我是内容<br />我是内容<br />我是内容<br />我是内容<br />
+      </div>
+    </div>
+    <!--priview-->
+    <!--code-->
+    <div style="width:800px;">
+      <script
+        type="text/plain"
+        class="language-markup"
+      >
+        <div class="demo9_3">
+            我是内容<br />我是内容<br />我是内容<br />我是内容<br />我是内容<br />我是内容<br />我是内容<br />我是内容<br />
+            我是内容<br />我是内容<br />我是内容<br />我是内容<br />我是内容<br />我是内容<br />我是内容<br />我是内容<br />
+        </div>
+      </script>
+
+      <pre><code class="language-scss">
+        .demo9_3{
+            width: 200px;
+            max-height: 200px;
+            overflow: auto;
+        }
+
+        .demo9_3 {
+            &::-webkit-scrollbar {
+                width: 10px;
+                background-color: #f5f5f5;
+            }
+            &::-webkit-scrollbar-thumb {
+                border-radius: 10px;
+                background: linear-gradient(to left, #fff, #e4e4e4);
+                border: 1px solid #aaa;
+            }
+            &::-webkit-scrollbar-track {
+                border-radius: 10px;
+                background: rgba(0,0,0,0.1);
+                border: 1px solid #ccc;
+            }
+        }
+      </code></pre>
+
+    </div>
+    <!--code-->
+    <back-to-top
+      :custom-style="myBackToTopStyle"
+      :visibility-height="300"
+      :back-position="50"
+      transition-name="fade"
+    />
+  </div>
+</template>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+@import "@/styles/layout-common.scss";
+</style>
+<script>
+import Prism from "prismjs";
+import BackToTop from "@/components/BackToTop";
+
+export default {
+  components: { BackToTop },
+  data() {
+    return {
+      myBackToTopStyle: {
+        right: "50px",
+        bottom: "50px",
+        width: "40px",
+        height: "40px",
+        "border-radius": "4px",
+        "line-height": "45px", // 请保持与高度一致以垂直居中 Please keep consistent with height to center vertically
+        background: "#e7eaf1" // 按钮的背景颜色 The background color of the button
+      }
+    };
+  },
+  mounted() {
+    Prism.highlightAll();
+  },
+  methods: {}
+};
+</script>
